08c1677b26969632adcdabf1f01bcfcba80ae03c
Kevin Ilsen <kevin@ilsen.net>
2023-12-05 09:08:55 -0500

    navbar collapse

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {NavLink} from 'react-router-dom';
 import classNames from 'classnames';
 
 const Header = () => {
+  const navButtonRef = useRef(null);
+  const linksContainerRef = useRef(null);
+
+  function collapseNav() {
+    navButtonRef.current.classList.add('collapsed');
+    linksContainerRef.current.classList.remove('in');
+  }
+
   return (
     <section id="header">
       <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -13,28 +21,29 @@ const Header = () => {
               className="navbar-toggle"
               type="button"
               data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1"
+              data-target="#lm-navbar"
+              ref={navButtonRef}
             >
               <span className="sr-only">Toggle navigation</span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <NavLink className="navbar-brand" to="/">
+            <NavLink className="navbar-brand" to="/" onClick={collapseNav}>
               <img
                 alt="Love Medicine"
                 src="/img/logo-wide.png"
               />
             </NavLink>
           </div>
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <div className="collapse navbar-collapse" id="lm-navbar" ref={linksContainerRef}>
             <ul className="nav navbar-nav navbar-right">
-              <li><NavLink to="/" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})}>HOME</NavLink></li>
-              <li><NavLink to="shows" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})}>SHOWS</NavLink></li>
-              <li><NavLink to="meet" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})}>MEET THE BAND</NavLink></li>
-              <li><NavLink to="gallery" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})}>GALLERY</NavLink></li>
-              <li><NavLink to="epk" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})}>EPK</NavLink></li>
-              <li><NavLink to="contact" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})}>CONTACT US</NavLink></li>
+              <li><NavLink to="/" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})} onClick={collapseNav}>HOME</NavLink></li>
+              <li><NavLink to="shows" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})} onClick={collapseNav}>SHOWS</NavLink></li>
+              <li><NavLink to="meet" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})} onClick={collapseNav}>MEET THE BAND</NavLink></li>
+              <li><NavLink to="gallery" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})} onClick={collapseNav}>GALLERY</NavLink></li>
+              <li><NavLink to="epk" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})} onClick={collapseNav}>EPK</NavLink></li>
+              <li><NavLink to="contact" className={({isActive}) => classNames({'nav-link': true, 'active': isActive})} onClick={collapseNav}>CONTACT US</NavLink></li>
             </ul>
           </div>
           {/* /.navbar-collapse*/}
